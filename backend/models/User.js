7b1cf6fc6871dbe478631d8cb2f6a24a519295e5
PalsAnalytix <palsanalytix.dev@gmail.com>
2024-10-07 14:57:05 +0530

    checking

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  auth0ID: { type: String, required: true, unique: true }, // Changed to match your route handler
+  auth0ID: { type: String, required: true, unique: true },
+  // Changed to match your route handler checking git hub
 });
 
 const User = mongoose.model('User', userSchema);
